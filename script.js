9ceb52ecbd7a3ce55a6e66efbb3045d080663302
Ah-Med702 <113292428+Ah-Med702@users.noreply.github.com>
2023-07-10 09:04:34 +0300

    put element to complete load

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-let span = document.querySelector(".container span");
-let result = document.querySelector(".container .result");
+let spanResultSpan = document.querySelector(".container span .result");
+let spanPlaceholder = document.querySelector(".container span .span-placeholder");
+let divResultSpan = document.querySelector(".container .result-div .result");
+let divPlaceholder = document.querySelector(".container .result-div .div-placeholder");
 let btn = document.querySelector(".container .btn");
 
 function fetchData() {
+
+    divResultSpan.insertAdjacentElement("afterend", divPlaceholder);
+    spanResultSpan.insertAdjacentElement("afterend", spanPlaceholder);
+
     let proId = Math.floor(Math.random() * 200);
-    fetch(`https://api.adviceslip.com/advice/${proId}`).then((response) => {
-        response.json().then((data) => {
-            span.innerHTML = `ADVICE #${data.slip.id}`;
-            result.innerHTML = `❝ ${data.slip.advice} ❞`;
+
+    fetch(`https://api.adviceslip.com/advice/${proId}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            const mySpan = document.createElement("div");
+            mySpan.innerHTML = `ADVICE #${data.slip.id}`;
+
+            const myElement = document.createElement("div");
+            myElement.innerHTML = data.slip.advice;
+
+            spanPlaceholder.replaceWith(mySpan);
+            divPlaceholder.replaceWith(myElement);
         });
-    });
-}fetchData();
+}
+fetchData();
 
 btn.addEventListener("click", () => {
-    fetchData()
+    fetchData();
 });
